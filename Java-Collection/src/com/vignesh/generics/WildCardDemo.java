e2b34edb6cda6a6c1ba345851bda30a3cf36ecab
vignesh-vicky2812 <116265109+vignesh-vicky2812@users.noreply.github.com>
2023-02-03 20:15:17 +0530

    lower wildcard

--- Java-Collection/src/com/vignesh/generics/WildCardDemo.java
+++ Java-Collection/src/com/vignesh/generics/WildCardDemo.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class WildCardDemo {
 
-	public static void displayData(List<?> l) {  //we dont know what generic
+//	public static void displayData(List<?> l) {  //we dont know what generic
+	public static void displayData(List<? super Integer> l) {
 		for(Object t: l) {
 			System.out.println(t);
 		}		
@@ -13,9 +14,15 @@ public class WildCardDemo {
 	
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>();
+		List<Number> list1 = new ArrayList<>();
 		
 		list.add(34);
 		list.add(50);
+		list1.add(22);
+		list1.add(2);
+		
+		displayData(list);
+		displayData(list1);
 		
 	}
 }
